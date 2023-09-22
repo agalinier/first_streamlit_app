b4e81162a0e4f4c84780699534e041f5965cb0b3
agalinier <144342654+agalinier@users.noreply.github.com>
2023-09-22 14:13:01 +0200

    Update streamlit_app.py
    add second choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,3 +40,6 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit list contains:")
 streamlit.dataframe(my_data_rows)
 
+# add new entry box
+add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfruit')
+
